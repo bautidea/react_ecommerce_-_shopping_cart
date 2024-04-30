b3d262bd08825ed91efd84790ea5b74c06f285e0
bautidea <juanbautistadeangelis@gmail.com>
2024-04-30 09:36:08 -0300

    styling cart

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -8,7 +8,14 @@ const Cart = ({ cartItems, isCartVisible }) => {
           <aside className="cartDisplay">
             <ul>
               {cartItems.map((cartProduct) => (
-                <li key={cartProduct.id}></li>
+                <li key={cartProduct.id}>
+                  <h3>{cartProduct.title}</h3>
+                  <img
+                    src={cartProduct.thumbnail}
+                    alt={`${cartProduct.brand} ${cartProduct.category}`}
+                  />
+                  <h4>{`${cartProduct.quantity} x $${cartProduct.price}`}</h4>
+                </li>
               ))}
             </ul>
           </aside>
