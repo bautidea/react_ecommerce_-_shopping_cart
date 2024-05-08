bad322427f60bc6f3af57c51e04e1430ed9cf660
bautidea <juanbautistadeangelis@gmail.com>
2024-05-08 11:47:53 -0300

    Starting to clean cartContext by implementing useReducer.

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -1,32 +1,44 @@
-import { createContext, useState } from 'react';
+import { createContext, useReducer, useState } from 'react';
 
 export const CartContext = createContext();
 
+const initialCartState = [];
+
+function cartReducer(state, action) {
+  switch (action.type) {
+    case 'ADD_TO_CART': {
+      // Finding index of the product in the cart.
+      // If product not in cart the it will return -1.
+      const productInIndex = state.findIndex(
+        (cartProduct) => cartProduct.id === action.product.id
+      );
+
+      // If product not on car im going to add it.
+      if (productInIndex === -1) {
+        return [...state, { ...action.product, quantity: 1 }];
+      } else {
+        return state.map((cartProduct) => {
+          if (cartProduct.id === action.product.id) {
+            return { ...cartProduct, quantity: cartProduct.quantity + 1 };
+          } else {
+            return { ...cartProduct };
+          }
+        });
+      }
+    }
+  }
+}
+
 export function CartContextProvider({ children }) {
-  const [cartItems, setCartItems] = useState([]);
+  const [cartItems, dispatch] = useReducer(cartReducer, initialCartState);
+  // const [cartItems, setCartItems] = useState([]);
   const [isCartVisible, setIsCartVisible] = useState(false);
 
   function addToCart(product) {
-    // Finding index of the product in the cart.
-    // If product not in cart the it will return -1.
-    const productInIndex = cartItems.findIndex(
-      (cartProduct) => cartProduct.id === product.id
-    );
-
-    // If product not on car im going to add it.
-    if (productInIndex === -1) {
-      setCartItems((prevState) => [...prevState, { ...product, quantity: 1 }]);
-    } else {
-      const newCart = cartItems.map((cartProduct) => {
-        if (cartProduct.id === product.id) {
-          return { ...cartProduct, quantity: cartProduct.quantity + 1 };
-        } else {
-          return { ...cartProduct };
-        }
-      });
-
-      setCartItems(newCart);
-    }
+    dispatch({
+      type: 'ADD_TO_CART',
+      product: product,
+    });
   }
 
   function updateInputProductQuantity(productId, inputQuantity) {
